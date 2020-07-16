c4e4c9181cb3e860b72d42e500c0f2a0601d2cb3
Evgeniy <evgeniy.ostapenko.1991@mail.ru>
2020-07-16 22:38:06 +0300

    App_Site_2

--- src/main/java/com/example/app_site/exception/ExceptionController.java
+++ src/main/java/com/example/app_site/exception/ExceptionController.java
@@ -2,6 +2,7 @@ package com.example.app_site.exception;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
@@ -9,6 +10,7 @@ public class ExceptionController implements ErrorController {
 
     @Override
     @ResponseBody
+    @RequestMapping("/error")
     public String getErrorPath() {
         return "No Mapping Found";
     }
